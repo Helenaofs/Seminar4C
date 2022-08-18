1a31ec373a9655aee03eae0cc2f9946d425ea97b
«helenaofs» <lilklinik@yandex.ru>
2022-08-19 00:24:00 +0700

    Task 1 added

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+// Напишите цикл, который принимает на вход два числа(A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.WriteLine("Введите два числа:");
+int[] array = new int[2];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int PowNumber(int num1, int num2)
+{
+    int result = num1;
+    for (int i = 1; i < num2; i++)
+    {
+        result = result * num1;
+    }
+    return result;
+}
+
+Console.WriteLine(PowNumber(array[0], array[1]));
